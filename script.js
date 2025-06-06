dbf8c02dd18a5f3a464f82e0671b1e4022c2e855
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-06 16:51:49 +0200

    Add form fields and buttons for train ticket calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("HELLO WORLD");
+
+const firstName = document.getElementById("first_name");
+const secondName = document.getElementById("second_name");
+const addKm = document.getElementById("add_kilometers");
+const ageRange = document.getElementById("age_range");
+
+const calcBtn = document.getElementById("calc_btn");
+const cancBtn = document.getElementById("cancel_btn");
+
+calcBtn.addEventListener("click", () => {
